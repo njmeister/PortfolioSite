c0ca9fa9f092f26d8f088c75b8a29ffa14e4b170
meistercraft <njmeister99@gmail.com>
2024-05-16 11:12:33 -0500

    added functionality to highlight the current page in the nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Nav from './Nav';
 
 export default function Header() {
+	const [currentPage, setCurrentPage] = useState('About');
+
+	const handlePageChange = (page) => setCurrentPage(page);
+
   return (
 	<header className="header">
 	  <h1><Link to="/">Noah Meister</Link></h1>
-	  <Nav />
+	  <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
 	</header>
   );
 }
