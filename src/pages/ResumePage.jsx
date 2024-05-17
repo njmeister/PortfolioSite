124bbb62660816aa4e4fd78a33f2b6732bfaa446
meistercraft <njmeister99@gmail.com>
2024-05-16 21:37:46 -0500

    added links to footer

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,13 +1,18 @@
 import Row from '../components/Row';
 
+const title = 'Proficiencies';
+const description = 'HTML, CSS, JavaScript, MongoDB, Express, React, Node.js, SQL';
+
 
 export default function ResumePage() {
+	const resume = '../../assets/images/Meister_Noah_Resume.pdf';
+
 	return (
 		<div>
-			<Row />
-			<div className="resume">
-				<img src="https://i.imgur.com/1Z2H1YH.png" alt="resume" />
-			</div>
+			<Row title={title} description={description} />
+			<a href={resume} download="Meister_Noah_Resume.pdf" className="downloadBtn">
+				<button>Download Resume</button>
+			</a>
 		</div>
 	);
 }
